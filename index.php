0017f12aa641b1aa01cba24804ac825b618c35f8
Bbillionaires <88366222+Bbillionaires@users.noreply.github.com>
2025-10-27 23:52:43 +0000

    Fix include_once syntax for PHP

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 include_once("conn.php");
 include_once("includes.php");
 
-include_once(__DIR__ "/templates/HeaderTemplate.php");
+include_once(__DIR__ . "/templates/HeaderTemplate.php");
 
 $q1 = "select * from dd_catalog, dd_categories where dd_catalog.catid = dd_categories.catid order by RANDOM() LIMIT 1";
 $r1 = $pdo->query($q1) or die(mysql_error());
 
 
 
-include_once(__DIR__ "/templates/IndexTemplate.php");
-include_once(__DIR__ "/templates/FooterTemplate.php");
+include_once(__DIR__ . "/templates/IndexTemplate.php");
+include_once(__DIR__ . "/templates/FooterTemplate.php");
 
 ?>
