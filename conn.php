88c7758598330ef0e01f6c27499b9bfd058bca57
Bbillionaires <88366222+Bbillionaires@users.noreply.github.com>
2025-10-28 21:58:22 +0000

    Wire PDO conn, integer active, save_item handler, set sequence default

--- conn.php
+++ conn.php
@@ -1,12 +1,12 @@
 <?php
 declare(strict_types=1);
 
-$dsn = 'pgsql:host=dpg-d3vl0oer433s73cvgcq0-a.oregon-postgres.render.com;port=5432;dbname=attorneydb;sslmode=require';
+$dsn  = 'pgsql:host=dpg-d3vl0oer433s73cvgcq0-a.oregon-postgres.render.com;port=5432;dbname=attorneydb;sslmode=require';
 $user = 'attorneydirectory';
 $pass = 'EOEVxdVxunGCo7OsJxr0wFNizAiSamEZ';
 
 $pdo = new PDO($dsn, $user, $pass, [
-  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-  PDO::ATTR_EMULATE_PREPARES => false,
+  PDO::ATTR_EMULATE_PREPARES   => false,
 ]);
