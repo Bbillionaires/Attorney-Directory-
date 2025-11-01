9fbc4ddeda5741d86788336813f3cfaf4cf9b7a8
De'Aris Henry <bbillionaires@gmail.com>
2025-11-01 15:42:50 +0000

    Hotfix: serve static index to avoid 502 while fixing DB

--- index_static.php
+++ index_static.php
@@ -1,8 +1,6 @@
-<?php
-?><!doctype html>
-<html><head><meta charset="utf-8"><title>Attorney Directory – Static</title></head>
-<body>
+<!doctype html><meta charset="utf-8">
+<title>Attorney Directory</title>
 <h1>It works 🎉</h1>
 <p>This is the static index page served without any DB calls.</p>
 <p>Health: <a href="/healthz.php">/healthz.php</a></p>
-</body></html>
+<p>DB test: <a href="/dbtest.php">/dbtest.php</a></p>
