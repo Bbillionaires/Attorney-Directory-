50fd210c739fb9a9266a7b0c2c6e168e519f4f98
De'Aris Henry <bbillionaires@gmail.com>
2025-11-02 00:54:37 +0000

    add which.php

--- which.php
+++ which.php
@@ -1,5 +1,1 @@
-<?php
-header('Content-Type: text/plain; charset=UTF-8');
-echo "WHICH FILE: ", __FILE__, "\n";
-echo "URI: ", ($_SERVER['REQUEST_URI'] ?? ''), "\n";
-echo "TIME: ", gmdate('c'), "\n";
+<?php header('Content-Type: text/plain'); echo __FILE__,"\n";
